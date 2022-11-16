c23ff24a7fa072505df81722bdd1e833c89c4882
Glenda Leonard <gleonard@mozilla.com>
2022-11-16 17:56:00 -0500

    DENG-509 added processing_period_offset to configuration

--- analysis/configuration/processing_dates.py
+++ analysis/configuration/processing_dates.py
@@ -34,6 +34,17 @@ def calculate_date_ranges(
     current_period = dataset_config.current_period
     period_offset = dataset_config.period_offset
 
+    # Normally there is a lag of 1 day from the date provided by Airflow to the fully available
+    # dataset (e.g Airflow DAG run on 2022-11-30 will include data up to the end of day 2022-11-29
+    # since 2022-11-30 will only be partially complete).  Some tables, however have a 2 day lag.
+    # To account for this the parameter `processing_period_offset` is used to shift the processing
+    # periods back by the specified number of days.  e.g.  If processing_period_offset=1 and the
+    # Airflow provided data param in 202-11-30, then the processing will only include data up to
+    # the end of 2022-11-28.
+    exclusive_end_date = exclusive_end_date - timedelta(
+        days=dataset_config.processing_period_offset
+    )
+
     # the number of days between the end of the baseline date range and the start of the current
     # date range, may be negative if they overlap
     dataset_gap = period_offset - baseline_period
--- tests/test_processing_dates.py
+++ tests/test_processing_dates.py
@@ -5,6 +5,8 @@ from analysis.configuration.processing_dates import calculate_date_ranges
 
 def test_calculate_date_ranges(mock_analysis_profile):
     airflow_date = datetime.strptime("2022-04-17", "%Y-%m-%d").replace(tzinfo=pytz.utc)
+    # resetting to 0 to verify no offset works.
+    mock_analysis_profile.dataset.processing_period_offset = 0
     mock_analysis_profile.dataset.period_offset = 7
     mock_analysis_profile.dataset.baseline_period = 7
     mock_analysis_profile.dataset.current_period = 7
@@ -30,11 +32,11 @@ def test_calculate_date_ranges_overlapping(mock_analysis_profile):
         mock_analysis_profile.dataset, airflow_date
     )
 
-    assert baseline_period.start_date.strftime("%Y-%m-%d") == "2022-04-04"
-    assert baseline_period.end_date.strftime("%Y-%m-%d") == "2022-04-11"
+    assert baseline_period.start_date.strftime("%Y-%m-%d") == "2022-04-03"
+    assert baseline_period.end_date.strftime("%Y-%m-%d") == "2022-04-10"
 
-    assert current_period.start_date.strftime("%Y-%m-%d") == "2022-04-10"
-    assert current_period.end_date.strftime("%Y-%m-%d") == "2022-04-17"
+    assert current_period.start_date.strftime("%Y-%m-%d") == "2022-04-09"
+    assert current_period.end_date.strftime("%Y-%m-%d") == "2022-04-16"
 
 
 def test_calculate_date_ranges_with_gap(mock_analysis_profile):
@@ -47,11 +49,11 @@ def test_calculate_date_ranges_with_gap(mock_analysis_profile):
         mock_analysis_profile.dataset, airflow_date
     )
 
-    assert baseline_period.start_date.strftime("%Y-%m-%d") == "2022-04-09"
-    assert baseline_period.end_date.strftime("%Y-%m-%d") == "2022-04-16"
+    assert baseline_period.start_date.strftime("%Y-%m-%d") == "2022-04-08"
+    assert baseline_period.end_date.strftime("%Y-%m-%d") == "2022-04-15"
 
-    assert current_period.start_date.strftime("%Y-%m-%d") == "2022-04-23"
-    assert current_period.end_date.strftime("%Y-%m-%d") == "2022-04-30"
+    assert current_period.start_date.strftime("%Y-%m-%d") == "2022-04-22"
+    assert current_period.end_date.strftime("%Y-%m-%d") == "2022-04-29"
 
 
 def test_calculate_date_ranges_single_day(mock_analysis_profile):
@@ -64,8 +66,8 @@ def test_calculate_date_ranges_single_day(mock_analysis_profile):
         mock_analysis_profile.dataset, airflow_date
     )
 
-    assert baseline_period.start_date.strftime("%Y-%m-%d") == "2022-04-02"
-    assert baseline_period.end_date.strftime("%Y-%m-%d") == "2022-04-03"
+    assert baseline_period.start_date.strftime("%Y-%m-%d") == "2022-04-01"
+    assert baseline_period.end_date.strftime("%Y-%m-%d") == "2022-04-02"
 
-    assert current_period.start_date.strftime("%Y-%m-%d") == "2022-04-16"
-    assert current_period.end_date.strftime("%Y-%m-%d") == "2022-04-17"
+    assert current_period.start_date.strftime("%Y-%m-%d") == "2022-04-15"
+    assert current_period.end_date.strftime("%Y-%m-%d") == "2022-04-16"
