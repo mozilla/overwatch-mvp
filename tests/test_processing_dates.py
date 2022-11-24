444607108838e6f089fc79c401a00ed61da6b2e0
Glenda Leonard <gleonard@mozilla.com>
2022-11-24 11:48:32 -0500

    Added test for processing_period_offset

--- tests/test_processing_dates.py
+++ tests/test_processing_dates.py
@@ -71,3 +71,30 @@ def test_calculate_date_ranges_single_day(mock_analysis_profile):
 
     assert current_period.start_date.strftime("%Y-%m-%d") == "2022-04-15"
     assert current_period.end_date.strftime("%Y-%m-%d") == "2022-04-16"
+
+
+def test_processing_period_offset(mock_analysis_profile):
+    airflow_date = datetime.strptime("2022-04-17", "%Y-%m-%d").replace(tzinfo=pytz.utc)
+    # resetting to 0 to verify 0 offset works.
+    mock_analysis_profile.dataset.processing_period_offset = 0
+    mock_analysis_profile.dataset.period_offset = 7
+    mock_analysis_profile.dataset.baseline_period = 7
+    mock_analysis_profile.dataset.current_period = 7
+
+    baseline_period, current_period = calculate_date_ranges(
+        mock_analysis_profile.dataset, airflow_date
+    )
+    assert baseline_period.start_date.strftime("%Y-%m-%d") == "2022-04-03"
+    assert baseline_period.end_date.strftime("%Y-%m-%d") == "2022-04-10"
+    assert current_period.start_date.strftime("%Y-%m-%d") == "2022-04-10"
+    assert current_period.end_date.strftime("%Y-%m-%d") == "2022-04-17"
+
+    # Set offset period to 3 days, all dates shift back by 3 days.
+    mock_analysis_profile.dataset.processing_period_offset = 3
+    baseline_period, current_period = calculate_date_ranges(
+        mock_analysis_profile.dataset, airflow_date
+    )
+    assert baseline_period.start_date.strftime("%Y-%m-%d") == "2022-03-31"
+    assert baseline_period.end_date.strftime("%Y-%m-%d") == "2022-04-07"
+    assert current_period.start_date.strftime("%Y-%m-%d") == "2022-04-07"
+    assert current_period.end_date.strftime("%Y-%m-%d") == "2022-04-14"
